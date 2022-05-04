69f554b9b5941f384ebf9b533874f628503fb567
JPScripter <JPScripter@gmail.com>
2022-05-04 08:55:45 -0500

    impersonation demo

--- ImpersonationDemo.ps1
+++ ImpersonationDemo.ps1
@@ -20,8 +20,17 @@ Invoke-DbaQuery -SqlInstance CM1 -Database ConfigMgr_chq -Query 'select suser_na
 
 
 
+#working Cross domain
+ping gw1
+Get-ChildItem -Path \\gw1\c$
+$Lab = get-Credential -UserName 'corp\labadmin' -Message 'lab admin'
+Set-impersonation -credential $lab -netonly
+
+
+
 <#
 
 1 - Only works with current thread
 2 - does not pass to child processes
 3 - whoami is weird
+#>
