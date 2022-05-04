14dcc6b74ba58fa38e47439d8ac09bea9c7b4f5b
JPScripter <JPScripter@gmail.com>
2022-05-04 08:50:25 -0500

    impersonation

---
+++ ImpersonationDemo.ps1
@@ -0,0 +1,27 @@
+$Current = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+$Current.name
+
+
+
+#Impersonation
+[System.Security.Principal.WindowsIdentity]::Impersonate
+Get-ChildItem HKLM:\SECURITY
+$MachineToken = Get-MachineToken 
+Set-Impersonation -Token $MachineToken
+Get-ChildItem HKLM:\SECURITY
+Set-Impersonation
+
+Invoke-DbaQuery -SqlInstance CM1 -Database ConfigMgr_chq -Query 'select suser_name()'
+$Lab = get-Credential -UserName 'corp\labadmin' -Message 'lab admin'
+Set-impersonation -credential $lab 
+Invoke-DbaQuery -SqlInstance CM1 -Database ConfigMgr_chq -Query 'select suser_name()'
+Set-Impersonation 
+Invoke-DbaQuery -SqlInstance CM1 -Database ConfigMgr_chq -Query 'select suser_name()'
+
+
+
+<#
+
+1 - Only works with current thread
+2 - does not pass to child processes
+3 - whoami is weird
